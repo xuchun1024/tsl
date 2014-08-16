db78ebd3894ec5f75768247f156b781b48886098
yuexiaojun <yuexiaojun@meituan.com>
2014-08-16 15:25:04 +0800

    add user by id

--- src/main/java/com/tosla/controller/UserController.java
+++ src/main/java/com/tosla/controller/UserController.java
@@ -9,14 +9,13 @@ import com.tosla.domain.User;
 import com.tosla.service.UserService;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * TODO 在这里编写类的功能描述
@@ -31,6 +30,8 @@ public class UserController {
 
     @Resource
     private UserService userService;
+
+    private Map<Integer,User> userMap;
 
     @RequestMapping(value = "/all",method = RequestMethod.GET,produces= MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -52,5 +53,34 @@ public class UserController {
         userList.add(user2);
         userService.printName();
         return userList;
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET,produces= MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Object getUser(@PathVariable String id) {
+
+
+        List<User> userList = new ArrayList<User>();
+        User user1 = new User();
+        user1.setId(1);
+        user1.setAddTime(new Date());
+        user1.setName("拉风少年");
+        user1.setPassword("123456");
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setAddTime(new Date());
+        user2.setName("千字");
+        user2.setPassword("123456");
+
+        userList.add(user1);
+        userList.add(user2);
+        userService.printName();
+
+        userMap = new HashMap<Integer, User>();
+        userMap.put(user1.getId(), user1);
+        userMap.put(user2.getId(), user2);
+
+        return userMap.get(id);
     }
 }
